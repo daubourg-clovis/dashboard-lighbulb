2e98b02902636f8c499e71c8608def186926cf44
daubourg-clovis <daubourg.clovis@gmail.com>
2020-06-23 15:53:06 +0200

    Update - preparation du tableau d'affichage des éléments

--- index.php
+++ index.php
@@ -10,6 +10,39 @@
     <title>Gestion des ampoules</title>
 </head>
 <body>
+    <h1>Ampoules</h1>
+    <p><a href="edit.php">Entrer un changement d'ampoule</a></p>
+    <table>
+        <tr>
+            <th>Date du Changement</th>
+            <th>Étage</th>
+            <th>Emplacement</th>
+            <th>Puissance et marque l'ampoule</th>
+            <th>Modifier</th>
+            <th>Supprimer</th>
+
+        </tr>
+        <?php 
+            //Preparation Requete
+            $sql = 'SELECT id, change_date, floor, position, power, brand FROM lighbulb';
+            $sth = $pdo->prepare($sql);
+            $sth->execute();
+            $datas = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+            //Preparation formatage des dates 
+            $intlDateFormatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
+            
+            //Affichage des élément voulus
+            foreach($datas as $data){
+                echo '<tr>';
+                echo '<td>'.$intlDateFormatter->format(strtotime($data['change_date'])).'</td>';
+                echo '<td>'.$data['floor'].'</td>';
+                echo '<td>'.$data['position'].'</td>';
+                echo '<td>'.$data['power'].' '.$data['brand'].'</td>';
+                echo '</tr>';
+            }
+        ?>
+    </table>
     
 </body>
 </html>
