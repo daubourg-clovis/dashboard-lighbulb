dc33977f385b52fe7773c6ac6f464e2a3dc9875f
daubourg-clovis <daubourg.clovis@gmail.com>
2020-06-23 16:39:00 +0200

    Update- ajout de lien modifier et supprimer vers nos pages

--- index.php
+++ index.php
@@ -17,14 +17,14 @@
             <th>Date du Changement</th>
             <th>Étage</th>
             <th>Emplacement</th>
-            <th>Puissance et marque l'ampoule</th>
+            <th>Puissance et marque de l'ampoule</th>
             <th>Modifier</th>
             <th>Supprimer</th>
 
         </tr>
         <?php 
             //Preparation Requete
-            $sql = 'SELECT id, change_date, floor, position, power, brand FROM lighbulb';
+            $sql = 'SELECT id, change_date, floor, position, power, brand FROM lightbulb';
             $sth = $pdo->prepare($sql);
             $sth->execute();
             $datas = $sth->fetchAll(PDO::FETCH_ASSOC);
@@ -32,13 +32,15 @@
             //Preparation formatage des dates 
             $intlDateFormatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
             
-            //Affichage des élément voulus
+            //Affichage des éléments
             foreach($datas as $data){
                 echo '<tr>';
                 echo '<td>'.$intlDateFormatter->format(strtotime($data['change_date'])).'</td>';
                 echo '<td>'.$data['floor'].'</td>';
                 echo '<td>'.$data['position'].'</td>';
                 echo '<td>'.$data['power'].' '.$data['brand'].'</td>';
+                echo '<td><a href="edit.php?edit=1&id='.$data['id'].'"> Modifier</a></td>';
+                echo '<td><a href="delete.php?id='.$data['id'].'">Supprimer</a></td>"';
                 echo '</tr>';
             }
         ?>
