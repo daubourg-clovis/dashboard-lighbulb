708b27c25ceb9d3dbc7a7e6cd1f608c15ca573c4
Clovis Daubourg <62110612+daubourg-clovis@users.noreply.github.com>
2020-06-24 09:39:25 +0200

    Update - création de requete pour supression de ligne

--- delete.php
+++ delete.php
@@ -1,2 +1,12 @@
 <?php
-    
+    require_once('db.php');
+
+    if(isset($_GET['id'])){
+        $sql = 'DELETE FROM lightbulb WHERE id=:id';
+        $sth = $pdo->prepare($sql);
+        $sth->bindParam(':id', $_GET['id'], PDO::PARAM_INT);
+        $sth->execute();
+
+    }
+
+    header('Location: index.php');
