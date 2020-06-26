208c3318a3a1e7227f842731c9e0d82d342e2ca3
daubourg-clovis <daubourg.clovis@gmail.com>
2020-06-26 14:16:53 +0200

    Update- ajout form bootstrap

--- edit.php
+++ edit.php
@@ -89,98 +89,120 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="medias/favicon.ico" type="image/x-icon">
     <link rel="icon" href="medias/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>Ajout/Modification</title>
 </head>
 <body>
-    <?php
-        if(isset($_GET['edit']) && ($_GET['id'])){
-            $titleText = "Modifier la ligne";
-        }else{
-            $titleText = "Ajouter une ampoule";
-        }
-    ?>
-    <h1><?=$titleText?></h1>
+    <div class="box">
 
-    <div>
-        <form action="" method="post">
-            <div>
-                <label for="change_date">Date du changement de l'ampoule :</label>
-                <input type="date" id="change_date" name="change_date" value="<?=$change_date?>">
-            </div>
-            <div>
-                <label for="floor">Étage :</label>
-                <select name="floor" id="floor" >
-                    <?php 
-                        if(isset($_GET['id']) && isset($_GET['edit'])){
-                            $value = $floor;
-                            $selectText = $floor;
-                        }else{
-                            $value = "";
-                            $selectText = "Choisissez un étage";
-                        }
-                    ?>
-                    <option value="<?=$value?>"><?=$selectText?></option>
-                    <option value="Premier étage">Premier étage</option>
-                    <option value="Deuxième étage">Deuxième étage</option>
-                    <option value="Troisième étage">Troisième étage</option>
-                    <option value="Quatrième étage">Quatrième étage</option>
-                    <option value="Cinquième étage">Cinquième étage</option>
-                    <option value="Sixième étage">Sixième étage</option>
-                    <option value="Septième étage">Septième étage</option>
-                    <option value="Huitième étage">Huitième étage</option>
-                    <option value="Neuvième étage">Neuvième étage</option>
-                    <option value="Dixième étage">Dixième étage</option>
-                    <option value="Onzième étage">Onzième étage</option>
-                </select>
- 
-            </div>
-            <div>
-                <label for="position">Coté du couloir :</label>
-                <select name="position" id="position">
-                <?php 
-                        if(isset($_GET['id']) && isset($_GET['edit'])){
-                            $value = $position;
-                            $selectText = $position;
-                        }else{
-                            $value = "";
-                            $selectText = "Choisissez un emplacement";
-                        }
-                    ?>
-                    <option value="<?=$value?>"><?=$selectText?></option>
-                    <option value="Côté droit">Côté droit</option>
-                    <option value="Côté gauche">Côté gauche</option>
-                    <option value="Fond">Fond</option>
-                </select>
-            
-            </div>
-            <div>
-                <label for="power">Puissance et marque de l'ampoule :</label>
-                <input type="text" id="power" name="power" placeholder ="Puissance en Watts" value="<?=$power?>">
-                <input type="text" id="brand" name="brand" placeholder="Marque" value="<?=$brand?>">
-            </div>
-            <div>
-                <?php
-                    if(isset($_GET['id']) && isset($_GET['edit'])){
-                        $validateText = 'Modifier';
-                    }else{
-                        $validateText = 'Ajouter';
-                    }
-                ?>
-                <button type="submit"><?=$validateText?></button>
-                <?php
-                    if(isset($_GET['id']) && isset($_GET['edit'])){
-                ?>
-                <input type="hidden" name="edit" value="1">
-                <input type="hidden" name="id" value="<?=$id?>">
-                <?php 
-                    }
-                ?>
-            </div>
-        </form>
-    </div>
+        <?php
+            if(isset($_GET['edit']) && ($_GET['id'])){
+                $titleText = "Modifier la ligne";
+            }else{
+                $titleText = "Ajouter une ampoule";
+            }
+        ?>
+        <h1><?=$titleText?></h1>
+        <button><a href="index.php">Annuler</a></button>
     
+        <div>
+            <form action="" method="post">
+                <div class="form-group row">
+                    <div class="col-lg-2">
+                        <label for="change_date" class="col-form-label">Date du changement de l'ampoule :</label>
+                    </div>
+                    <div>
+                        <input type="date" id="change_date" name="change_date" value="<?=$change_date?>" required>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-lg-2">
+                        <label for="floor" class="col-form-label">Étage :</label>
+                    </div>
+                    <select name="floor" id="floor" required>
+                        <?php 
+                            if(isset($_GET['id']) && isset($_GET['edit'])){
+                                $value = $floor;
+                                $selectText = $floor;
+                            }else{
+                                $value = "";
+                                $selectText = "Choisissez un étage";
+                            }
+                        ?>
+                        <option value="<?=$value?>"><?=$selectText?></option>
+                        <option value="Premier étage">Premier étage</option>
+                        <option value="Deuxième étage">Deuxième étage</option>
+                        <option value="Troisième étage">Troisième étage</option>
+                        <option value="Quatrième étage">Quatrième étage</option>
+                        <option value="Cinquième étage">Cinquième étage</option>
+                        <option value="Sixième étage">Sixième étage</option>
+                        <option value="Septième étage">Septième étage</option>
+                        <option value="Huitième étage">Huitième étage</option>
+                        <option value="Neuvième étage">Neuvième étage</option>
+                        <option value="Dixième étage">Dixième étage</option>
+                        <option value="Onzième étage">Onzième étage</option>
+                    </select>
+     
+                </div>
+                <div class="form-group row">
+                    <div class="col-lg-2">
+                        <label for="position" class="col-form-label">Coté du couloir :</label>
+                    </div>
+                    <select name="position" id="position" required>
+                    <?php 
+                            if(isset($_GET['id']) && isset($_GET['edit'])){
+                                $value = $position;
+                                $selectText = $position;
+                            }else{
+                                $value = "";
+                                $selectText = "Choisissez un emplacement";
+                            }
+                        ?>
+                        <option value="<?=$value?>"><?=$selectText?></option>
+                        <option value="Côté droit">Côté droit</option>
+                        <option value="Côté gauche">Côté gauche</option>
+                        <option value="Fond">Fond</option>
+                    </select>
+                
+                </div>
+                <div class="form-group row" >
+                    <div class="col-lg-2">
+                        <label for="power" class="col-form-label">Puissance et marque de l'ampoule :</label>
+                    </div>
+                    <div>
+                        <input type="text" id="power" name="power" placeholder ="Puissance en Watts" value="<?=$power?>" required >
+                        <input type="text" id="brand" name="brand" placeholder="Marque" value="<?=$brand?>" required>
+                    </div>
+                </div>
+                <div>
+                    <?php
+                        if(isset($_GET['id']) && isset($_GET['edit'])){
+                            $validateText = 'Modifier';
+                        }else{
+                            $validateText = 'Ajouter';
+                        }
+                    ?>
+                    <button type="submit"><?=$validateText?></button>
+                    <?php
+                        if(isset($_GET['id']) && isset($_GET['edit'])){
+                    ?>
+                    <input type="hidden" name="edit" value="1">
+                    <input type="hidden" name="id" value="<?=$id?>">
+                    <?php 
+                        }
+                    ?>
+                </div>
+            </form>
+        </div>
+    </div>
+<script src="https://unpkg.com/@popperjs/core@2/dist/umd/popper.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -5,49 +5,57 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestion des ampoules</title>
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="medias/favicon.ico" type="image/x-icon">
     <link rel="icon" href="medias/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <title>Gestion des ampoules</title>
 </head>
 <body>
-<h1>Ampoules</h1>
-    <p><a href="edit.php"> Entrer un changement d'ampoule</a></p>
-    <table>
-        <tr>
-            <th>Date du Changement</th>
-            <th>Étage</th>
-            <th>Emplacement</th>
-            <th>Puissance et marque de l'ampoule</th>
-            <th>Modifier</th>
-            <th>Supprimer</th>
+    <div class="container">
 
-        </tr>
-        <?php 
-            //Preparation Requete
-            $sql = 'SELECT id, change_date, floor, position, power, brand FROM lightbulb';
-            $sth = $pdo->prepare($sql);
-            $sth->execute();
-            $datas = $sth->fetchAll(PDO::FETCH_ASSOC);
+        <h1>Gérer les ampoules de l'immeuble</h1>
+            <p><a href="edit.php"> Entrer un changement d'ampoule</a></p>
+            <table class="table table-bordered">
+                <tr>
+                    <th>Date du Changement</th>
+                    <th>Étage</th>
+                    <th>Emplacement</th>
+                    <th>Puissance et marque de l'ampoule</th>
+                    <th>Modifier</th>
+                    <th>Supprimer</th>
+        
+                </tr>
+                <?php 
+                    //Preparation Requete
+                    $sql = 'SELECT id, change_date, floor, position, power, brand FROM lightbulb';
+                    $sth = $pdo->prepare($sql);
+                    $sth->execute();
+                    $datas = $sth->fetchAll(PDO::FETCH_ASSOC);
+        
+                    //Preparation formatage des dates 
+                    $intlDateFormatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
+                    
+                    //Affichage des éléments
+                    foreach($datas as $data){
+                        echo '<tr>';
+                        echo '<td>'.$intlDateFormatter->format(strtotime($data['change_date'])).'</td>';
+                        echo '<td>'.$data['floor'].'</td>';
+                        echo '<td>'.$data['position'].'</td>';
+                        echo '<td>'.$data['power'].' '.$data['brand'].'</td>';
+                        echo '<td class="svg-center"><a href="edit.php?edit=1&id='.$data['id'].'"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="edit"><path d="M14.078 4.232l-12.64 12.639-1.438 7.129 7.127-1.438 12.641-12.64-5.69-5.69zm-10.369 14.893l-.85-.85 11.141-11.125.849.849-11.14 11.126zm2.008 2.008l-.85-.85 11.141-11.125.85.85-11.141 11.125zm18.283-15.444l-2.816 2.818-5.691-5.691 2.816-2.816 5.691 5.689z"/ ></svg></a></td>';
+                        echo '<td class="svg-center"><a href="delete.php?id='.$data['id'].'" onclick="return delFunction()"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="delete"><path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/ ></svg></a></td>';
+                        echo '<script> function delFunction(){ let del = confirm("Êtes vous sur de vouloir supprimer ?"); if (del == false){return false;}}</script>';
+                        echo '</tr>';
+                    }
+                ?>
+            </table>
+    </div>
+<script src="https://unpkg.com/@popperjs/core@2/dist/umd/popper.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
-            //Preparation formatage des dates 
-            $intlDateFormatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
-            
-            //Affichage des éléments
-            foreach($datas as $data){
-                echo '<tr>';
-                echo '<td>'.$intlDateFormatter->format(strtotime($data['change_date'])).'</td>';
-                echo '<td>'.$data['floor'].'</td>';
-                echo '<td>'.$data['position'].'</td>';
-                echo '<td>'.$data['power'].' '.$data['brand'].'</td>';
-                echo '<td><a href="edit.php?edit=1&id='.$data['id'].'"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M14.078 4.232l-12.64 12.639-1.438 7.129 7.127-1.438 12.641-12.64-5.69-5.69zm-10.369 14.893l-.85-.85 11.141-11.125.849.849-11.14 11.126zm2.008 2.008l-.85-.85 11.141-11.125.85.85-11.141 11.125zm18.283-15.444l-2.816 2.818-5.691-5.691 2.816-2.816 5.691 5.689z"/></svg></a></td>';
-                echo '<td><a href="delete.php?id='.$data['id'].'" onclick="return delFunction()"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/></svg></a></td>';
-                echo '<script> function delFunction(){ let del = confirm("Êtes vous sur de vouloir supprimer ?"); if (del == false){return false;}}</script>';
-                echo '</tr>';
-            }
-        ?>
-    </table>
-    
 </body>
 </html>
