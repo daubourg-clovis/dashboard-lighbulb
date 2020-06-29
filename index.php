4e6f5c037663680b3bf9cc2065a9a34b1138bc1d
daubourg-clovis <daubourg.clovis@gmail.com>
2020-06-29 15:39:33 +0200

    Update - transformation id to class

--- index.php
+++ index.php
@@ -47,11 +47,11 @@
                         
                         //Affichage des éléments
                         foreach($datas as $data){
-                            echo '<tr id="tr-hover">';
+                            echo '<tr class="tr-hover">';
                             echo '<td>'.$intlDateFormatter->format(strtotime($data['change_date'])).'</td>';
                             echo '<td>'.$data['floor'].'</td>';
                             echo '<td>'.$data['position'].'</td>';
-                            echo '<td id="pw-content">'.$data['power'].'</td>';
+                            echo '<td class="pw-content">'.$data['power'].'</td>';
                             echo '<td>'.$data['brand'].'</td>';
                             echo '<td class="svg-center"><a href="edit.php?edit=1&id='.$data['id'].'"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="edit"><path d="M14.078 4.232l-12.64 12.639-1.438 7.129 7.127-1.438 12.641-12.64-5.69-5.69zm-10.369 14.893l-.85-.85 11.141-11.125.849.849-11.14 11.126zm2.008 2.008l-.85-.85 11.141-11.125.85.85-11.141 11.125zm18.283-15.444l-2.816 2.818-5.691-5.691 2.816-2.816 5.691 5.689z"/></svg></a></td>';
                             echo '<td class="svg-center"><a href="delete.php?id='.$data['id'].'" onclick="return delFunction()"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="delete"><path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/></svg></a></td>';
